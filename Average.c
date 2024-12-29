c6958525745c63ce0ead9f9dcac62edbbb7a57f6
razib-develop <rabibk376@gmail.com>
2024-12-29 18:26:29 +0500

    avg

--- Average.c
+++ Average.c
@@ -6,8 +6,8 @@ int main()
     printf("Enter any four number different\n");
     scanf(" %d %d %d %d",&a,&b,&c,&d);
     sum=a+b+c+d;
-    printf("sum of the number is %d \n",sum);
+    printf("sum of the number = %d \n",sum);
     aver=sum/4;
-    printf("Average of the number is %d",aver);
+    printf("Average of the number = %d",aver);
     return 0;
 }    
